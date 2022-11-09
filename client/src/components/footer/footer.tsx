b195bcc870493578b9794fcf53e7698b9d6608bf
abdou059 <abdoumonuir9@gmail.com>
2022-11-09 09:29:28 +0200

    fix: edit footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './style.css';
 
@@ -6,6 +7,22 @@ function Footer() {
     <div
       className="footer"
     >
+      <Typography
+        variant="h6"
+        noWrap
+        sx={{
+          mr: 2,
+          display: { xs: 'none', md: 'flex' },
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          fontSize: '27px',
+          letterSpacing: '.3rem',
+          color: '#fff',
+          textDecoration: 'none',
+        }}
+      >
+        GoodCar
+      </Typography>
       <div style={{
         display: 'flex',
         width: '70%',
@@ -17,28 +34,29 @@ function Footer() {
         <div
           className="footerCont"
         >
-          <ol>
-            <h1>Used cars by brand</h1>
-            <Link to="/"><li>Toyota Cars</li></Link>
-            <Link to="/"><li>Toyota Cars</li></Link>
-            <Link to="/"><li>Nissan Cars</li></Link>
-            <Link to="/"><li>Ford Cars</li></Link>
-            <Link to="/"><li>BMW Cors</li></Link>
-          </ol>
-          <ol>
-            <h1>Used cars by Type</h1>
-            <Link to="/cars"><li>Toyota Cars </li></Link>
-            <Link to="/cars"><li>Nissan Cars </li></Link>
-            <Link to="/cars"><li>Ford Cars </li></Link>
-            <Link to="/cars"><li>BMW Cors </li></Link>
-          </ol>
-          <ol>
-            <h1>Used Cars by Year</h1>
-            <li>2020</li>
-            <li>2019</li>
-            <li>2018</li>
-            <li>2017</li>
-          </ol>
+          <div className="table">
+            <ol>
+              <h1>Used cars by brand</h1>
+              <Link to="/"><li>Toyota Cars</li></Link>
+              <Link to="/"><li>Nissan Cars</li></Link>
+              <Link to="/"><li>Ford Cars</li></Link>
+              <Link to="/"><li>BMW Cors</li></Link>
+            </ol>
+            <ol>
+              <h1>Used cars by Type</h1>
+              <Link to="/cars"><li>Toyota Cars </li></Link>
+              <Link to="/cars"><li>Nissan Cars </li></Link>
+              <Link to="/cars"><li>Ford Cars </li></Link>
+              <Link to="/cars"><li>BMW Cors </li></Link>
+            </ol>
+            <ol>
+              <h1>Used Cars by Year</h1>
+              <Link to="/cars"><li>2020 </li></Link>
+              <Link to="/cars"><li>2021 </li></Link>
+              <Link to="/cars"><li>2023 </li></Link>
+              <Link to="/cars"><li>2019 </li></Link>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
